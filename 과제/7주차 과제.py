86498897470584702264f8a8f03aeb0bf6356359
1a2a3a4a5 <8804who@naver.com>
2022-05-26 00:00:24 +0900

    수업 실습

---
+++ 과제/7주차 과제.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+#가
+data=pd.read_csv('D:/공부/비교과/USG/코딩엑스AI코스/실습/data/Student_Performance.csv')
+
+#나
+X=data[['math', 'reading', 'writing']]
+
+#다
+y=data['preparation']
+
+#마
+Knn_model=KNeighborsClassifier()
+Knn_model.fit(X, y)
+print(Knn_model.score(X, y))
+
+#바
+DecisionTreeModel=DecisionTreeClassifier(max_depth=3)
+DecisionTreeModel.fit(X,y)
+print(DecisionTreeModel.score(X,y))
